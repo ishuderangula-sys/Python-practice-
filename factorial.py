2b8ced72c3cbad3269a307251ab2defb4aa0fa41
Deragula Iswarya <ishuderangula@gmail.com>
2026-02-28 14:56:42 +0530

    Implement factorial calculation in factorial.py

---
+++ factorial.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial is not defined for negative numbers")
+elif num == 0:
+    print("Factorial is 1")
+else:
+    fact = 1
+    for i in range(1, num + 1):
+        fact = fact * i
+    print("Factorial of", num, "is", fact)
