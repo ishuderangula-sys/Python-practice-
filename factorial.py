bbe638af2d4b1a92e3fd11b4f9244dcea08fc353
Deragula Iswarya <ishuderangula@gmail.com>
2026-03-09 08:23:49 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -9,3 +9,12 @@ else:
     for i in range(1, num + 1):
         fact = fact * i
     print("Factorial of", num, "is", fact)
+    num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial not defined for negative numbers")
+else:
+    fact = 1
+    for i in range(1, num + 1):
+        fact = fact * i
+    print("Factorial:", fact)
